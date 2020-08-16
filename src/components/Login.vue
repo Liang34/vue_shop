7afed24874b0c6f92824e6fe89ce4a151ccde80d
Liang34 <51255027+Liang34@users.noreply.github.com>
2020-08-16 11:42:25 +0800

    第一次提交

---
+++ src/components/Login.vue
@@ -0,0 +1,24 @@
+<!-- 登录组件 -->
+<template>
+  <div>登录</div>
+</template>
+
+<script>
+
+export default {
+  data () {},
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+
+}
+
+</script>
+
+<style lang='scss' scoped>
+</style>
